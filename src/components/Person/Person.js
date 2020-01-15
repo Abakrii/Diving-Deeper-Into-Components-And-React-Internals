d5b62acf90e198f9251e91eaa588bf56cde63fd6
Abdelrahman Bakry <abdelrahmansalembakry@gmail.com>
2020-01-15 21:50:19 +0400

    A Better Component Structure

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -8,6 +8,8 @@ const person = ( props ) => {
     // if ( rnd > 0.7 ) {
     //     throw new Error( 'Something went wrong' );
     // }
+
+    console.log(props.children)
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
